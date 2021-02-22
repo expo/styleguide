2a4de0a339e640a76c704b6a2e6992673d6c5c97
jonsamp <sampjon@gmail.com>
2021-02-22 14:34:33 -0500

    cleanup

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import { shadows } from './src/shadows';
-import { theme, colors } from './src/theme';
+import { theme, colors, legacyColors } from './src/theme';
 import { BlockingSetInitialColorMode } from './src/theme/BlockingSetInitialColorMode';
 import { ThemeProvider, useTheme } from './src/theme/ThemeProvider';
 
 export * from './src/icons';
 export {
   colors,
+  legacyColors,
   shadows,
   theme,
   BlockingSetInitialColorMode,
--- src/theme/BlockingSetInitialColorMode.tsx
+++ src/theme/BlockingSetInitialColorMode.tsx
@@ -4,15 +4,10 @@ export function getInitialColorMode(): string | null {
   const preference = window.localStorage.getItem('data-expo-theme');
   let hasPreference = typeof preference === 'string';
 
-  /**
-   * If the user has explicitly chosen light or dark,
-   * use it. Otherwise, this value will be null.
-   */
   if (hasPreference) {
     return preference;
   }
 
-  // If there is no saved preference, use a media query
   const mql = window.matchMedia('(prefers-color-scheme: dark)');
 
   const systemPreference = typeof mql.matches === 'boolean';
@@ -20,7 +15,6 @@ export function getInitialColorMode(): string | null {
     return mql.matches ? 'dark' : 'light';
   }
 
-  // default to 'light'.
   return 'light';
 }
 
@@ -37,9 +31,9 @@ function setInitialColorMode() {
 
 // our function needs to be a string so that we can call it
 const blockingSetInitialColorMode = `(function() {
-        ${getInitialColorMode.toString()}
-        ${setInitialColorMode.toString()}
-        setInitialColorMode();
+  ${getInitialColorMode.toString()}
+  ${setInitialColorMode.toString()}
+  setInitialColorMode();
 })()
 `;
 
