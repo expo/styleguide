3bb37af6b43e19bd59e45981ae6a21ec8e4c6182
Bartosz Kaszubowski <gosimek@gmail.com>
2021-02-22 11:45:30 +0100

    add basic shadows for dark mode

--- src/shadows.ts
+++ src/shadows.ts
@@ -5,4 +5,9 @@ export const shadows = {
   large: '0 30px 60px rgba(0,0,0,0.12)',
   input: '0 2px 2px 0 rgba(0,0,20,0.015), 0 0 0 1px rgba(0,0,20,0.0075)',
   button: '0 2px 2px 0 rgba(0,0,20,0.015), 0 0 0 1px rgba(0,0,20,0.0075)',
+  
+  dark: {
+    popover: '0 2px 6px rgba(0,0,0,0.35)',
+    small: '0 5px 10px rgba(0,0,0,0.32)',
+  }
 };
