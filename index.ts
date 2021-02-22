15f38aca2760d3057e7c9995faff7770eee541f7
jonsamp <sampjon@gmail.com>
2021-02-22 13:20:17 -0500

    adds icons

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { theme, colors } from './src/theme';
 import { BlockingSetInitialColorMode } from './src/theme/BlockingSetInitialColorMode';
 import { ThemeProvider, useTheme } from './src/theme/ThemeProvider';
 
+export * from './src/icons';
 export {
   colors,
   shadows,
