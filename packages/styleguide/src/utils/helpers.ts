e6b428cc06b5974655bb5131e69827b442c0a56c
Simek <gosimek@gmail.com>
2022-05-02 13:43:51 +0200

    helper tweaks

--- packages/styleguide/src/utils/helpers.ts
+++ packages/styleguide/src/utils/helpers.ts
@@ -1,14 +1,14 @@
 // https://gist.github.com/paulirish/5558557
 export const isLocalStorageAvailable = () => {
   try {
-    if (!window.localStorage || localStorage === null || typeof localStorage === 'undefined') {
+    if (!window.localStorage || typeof window.localStorage === 'undefined') {
       return false;
     }
-    localStorage.setItem('localStorage:test', 'value');
-    if (localStorage.getItem('localStorage:test') !== 'value') {
+    window.localStorage.setItem('localStorage:test', 'value');
+    if (window.localStorage.getItem('localStorage:test') !== 'value') {
       return false;
     }
-    localStorage.removeItem('localStorage:test');
+    window.localStorage.removeItem('localStorage:test');
     return true;
   } catch {
     return false;
