7a76c46cac9b147de4ee78c192f9503e44bc3b2e
Simek <gosimek@gmail.com>
2023-01-13 14:34:16 +0100

    [styleguide] extend colors palette

--- examples/web/pages/colors.js
+++ examples/web/pages/colors.js
@@ -5,9 +5,16 @@ export default function Colors() {
   function renderPalette(colorName) {
     return (
       <div className='flex gap-4 flex-wrap'>
-        {Array.from(Array(12).keys()).map((index) => (
+        <div className='flex flex-col justify-around pb-4'>
+          <div>Theme</div>
+          <div>Light</div>
+          <div>Dark</div>
+        </div>
+        {Array.from(Array(14).keys()).map((index) => (
           <div key={index}>
-            <div style={{ backgroundColor: theme.palette[`${colorName}${index + 1}`]}} className="w-14 h-14 rounded-sm" />
+            <div style={{ backgroundColor: theme.palette[`${colorName}${index + 1}`] }} className="w-14 h-14 mb-2 rounded-sm" />
+            <div style={{ backgroundColor: palette.light[`${colorName}${index + 1}`] }} className="w-14 h-14 mb-2 rounded-sm" />
+            <div style={{ backgroundColor: palette.dark[`${colorName}${index + 1}`] }} className="w-14 h-14 rounded-sm" />
             <p className='text-3xs'>{colorName}{index+1}</p>
           </div>
         ))}
--- packages/styleguide/src/styles/palette.ts
+++ packages/styleguide/src/styles/palette.ts
@@ -1,48 +1,80 @@
-import { blue, blueDark, red, redDark, yellow, yellowDark, green, greenDark, orange, orangeDark, purple, purpleDark, pink, pinkDark, slate, slateDark } from '@radix-ui/colors'
+import {
+  blue, blueDark,
+  red, redDark,
+  yellow, yellowDark,
+  green, greenDark,
+  orange, orangeDark,
+  purple, purpleDark,
+  pink, pinkDark,
+  slate, slateDark
+} from '@radix-ui/colors'
+import tinycolor from "tinycolor2";
+
+function expandPalette(palette: any, colorName: string, isDarkMode = false) {
+  const { [`${colorName}12`]: lastColor, ...rest } = palette;
+  const expansion = {
+    [`${colorName}12`]: (isDarkMode ? tinycolor(lastColor).darken(18) : tinycolor(lastColor).lighten(18)).toString("hsl"),
+    [`${colorName}13`]: (isDarkMode ? tinycolor(lastColor).darken(9) : tinycolor(lastColor).lighten(9)).toString("hsl"),
+    [`${colorName}14`]: lastColor,
+  };
+  return Object.assign({}, rest, expansion);
+}
+
+const gray = {
+  gray1: slate.slate1,
+  gray2: slate.slate2,
+  gray3: slate.slate3,
+  gray4: slate.slate4,
+  gray5: slate.slate5,
+  gray6: slate.slate6,
+  gray7: slate.slate7,
+  gray8: slate.slate8,
+  gray9: slate.slate9,
+  gray10: slate.slate10,
+  gray11: slate.slate11,
+  gray12: 'hsl(206, 10%, 30%)',
+  gray13: 'hsl(206, 18%, 16%)',
+  gray14: slate.slate12,
+} as const;
+
+const grayDark = {
+  gray1: slateDark.slate1,
+  gray2: slateDark.slate2,
+  gray3: slateDark.slate3,
+  gray4: slateDark.slate4,
+  gray5: slateDark.slate5,
+  gray6: slateDark.slate6,
+  gray7: slateDark.slate7,
+  gray8: slateDark.slate8,
+  gray9: slateDark.slate9,
+  gray10: slateDark.slate10,
+  gray11: slateDark.slate11,
+  gray12: 'hsl(210, 6%, 73%)',
+  gray13: 'hsl(210, 6%, 83%)',
+  gray14: slateDark.slate12
+} as const;
 
 export const palette = {
   white: 'hsl(0, 0%, 100%)',
   black: 'hsl(0, 0%, 0%)',
   light: {
-    ...blue,
-    ...red,
-    ...yellow,
-    ...green,
-    ...orange,
-    ...purple,
-    ...pink,
-    gray1: slate.slate1,
-    gray2: slate.slate2,
-    gray3: slate.slate3,
-    gray4: slate.slate4,
-    gray5: slate.slate5,
-    gray6: slate.slate6,
-    gray7: slate.slate7,
-    gray8: slate.slate8,
-    gray9: slate.slate9,
-    gray10: slate.slate10,
-    gray11: slate.slate11,
-    gray12: slate.slate12
+    ...expandPalette(blue, "blue"),
+    ...expandPalette(red, "red"),
+    ...expandPalette(yellow, "yellow"),
+    ...expandPalette(green, "green"),
+    ...expandPalette(orange, "orange"),
+    ...expandPalette(purple, "purple"),
+    ...expandPalette(pink, "pink"),
+    ...gray,
   },
   dark: {
-    ...blueDark,
-    ...redDark,
-    ...yellowDark,
-    ...greenDark,
-    ...orangeDark,
-    ...purpleDark,
-    ...pinkDark,
-    gray1: slateDark.slate1,
-    gray2: slateDark.slate2,
-    gray3: slateDark.slate3,
-    gray4: slateDark.slate4,
-    gray5: slateDark.slate5,
-    gray6: slateDark.slate6,
-    gray7: slateDark.slate7,
-    gray8: slateDark.slate8,
-    gray9: slateDark.slate9,
-    gray10: slateDark.slate10,
-    gray11: slateDark.slate11,
-    gray12: slateDark.slate12
+    ...expandPalette(blueDark, "blue", true),
+    ...expandPalette(redDark, "red", true),
+    ...expandPalette(yellowDark, "yellow", true),
+    ...expandPalette(greenDark, "green", true),
+    ...expandPalette(orangeDark, "orange", true),
+    ...expandPalette(purpleDark, "purple", true),
+    ...expandPalette(pinkDark, "pink", true),
+    ...grayDark
   },
-};
+} as const;
