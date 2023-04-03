c517d3e9720c93dc6e74ac559b21029b805804a9
Bartosz Kaszubowski <gosimek@gmail.com>
2023-04-03 19:45:30 +0200

    debug cleanup

--- packages/example-web/pages/index.tsx
+++ packages/example-web/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <H1 className="prose-strong:text-secondary">
         @expo<strong>/</strong>styleguide
       </H1>
-      <p className="text-lg mt-8 prose-invert:">
+      <p className="text-lg mt-8">
         A collection of packages used to share styles and icons across{' '}
         <WordMarkLogo className="text-default w-auto h-6 inline relative -top-0.5 px-0.5" /> websites and projects.
       </p>
