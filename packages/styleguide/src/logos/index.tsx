ad349f50a18a9aa6999cc9e09fd9bb4b74fd04cc
Bartosz Kaszubowski <gosimek@gmail.com>
2023-08-31 16:54:39 +0200

    [styleguide] add Orbit logo (#69)

--- packages/styleguide/src/logos/index.tsx
+++ packages/styleguide/src/logos/index.tsx
@@ -1,6 +1,7 @@
 export { DocsLogo } from './DocsLogo';
 export { ExpoGoLogo } from './ExpoGoLogo';
 export { Logo } from './Logo';
+export { OrbitLogo } from './OrbitLogo';
 export { RouterLogo } from './RouterLogo';
 export { SnackLogo } from './SnackLogo';
 export { WordMarkLogo } from './WordMarkLogo';
