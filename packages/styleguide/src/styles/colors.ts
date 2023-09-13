a971ba499e981179a3c4c4512805398d45a8c88e
Simek <gosimek@gmail.com>
2023-09-13 21:17:20 +0200

    [styleguide] add app backgrounds to CSS variables and exported constants

---
+++ packages/styleguide/src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const appBackgroundColors = {
+  cyan: '#07c0cb',
+  lightBlue: '#1e92c4',
+  darkBlue: '#0b67af',
+  indigo: '#4b50b2',
+  purple: '#8945a3',
+  pink: '#c04891',
+  orange: '#e96d3c',
+  gold: '#f38f2f',
+  yellow: '#eebc01',
+  lime: '#aabd04',
+  lightGreen: '#6aa72a',
+  darkGreen: '#3a8e39',
+};
