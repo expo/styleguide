3ccef564eda6e4bfa41cbf2313971267c95713c8
Bartosz Kaszubowski <gosimek@gmail.com>
2023-09-14 09:37:49 +0200

    [styleguide] add app backgrounds to CSS variables and exported constants (#70)
    Co-authored-by: Juwan Wheatley <datwheat@gmail.com>

--- packages/styleguide/index.ts
+++ packages/styleguide/index.ts
@@ -1,6 +1,7 @@
 export * from './src/components';
 export * from './src/logos';
 export { shadows } from './src/styles/shadows';
+export { appBackgroundColors } from './src/styles/colors';
 export { theme } from './src/styles/themes';
 export { typography } from './src/styles/typography';
 export { mergeClasses } from './src/helpers/mergeClasses';
