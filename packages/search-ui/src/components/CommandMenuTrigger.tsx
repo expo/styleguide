def6ccba9502356a06863abd15bcd157f619cf76
Bartosz Kaszubowski <gosimek@gmail.com>
2023-11-10 19:54:15 +0100

    [search-ui] allow multiple triggers, style tweaks (#74)

--- packages/search-ui/src/components/CommandMenuTrigger.tsx
+++ packages/search-ui/src/components/CommandMenuTrigger.tsx
@@ -16,33 +16,19 @@ export const CommandMenuTrigger = ({ setOpen, className }: Props) => {
     setIsMac(typeof navigator !== 'undefined' && isAppleDevice());
   }, []);
 
-  useEffect(() => {
-    if (isMac !== null) {
-      const keyDownListener = (e: KeyboardEvent) => {
-        if (e.key === 'k' && (isMac ? e.metaKey : e.ctrlKey)) {
-          e.preventDefault();
-          setOpen((open) => !open);
-        }
-      };
-      document.addEventListener('keydown', keyDownListener, false);
-      return () => document.removeEventListener('keydown', keyDownListener);
-    }
-  }, [isMac]);
-
   return (
     <Button
-      id="cmdk-trigger"
       theme="secondary"
       leftSlot={<SearchSmIcon className="icon-md text-icon-secondary" />}
       rightSlot={
         isMac !== null ? (
-          <div className="ml-auto max-lg-gutters:hidden">
+          <div className="ml-auto max-md-gutters:hidden">
             <kbd className="!h-5 !leading-[19px]">{isMac ? '⌘' : 'Ctrl'}</kbd>{' '}
             <kbd className="!h-5 !leading-[19px]">K</kbd>
           </div>
         ) : undefined
       }
-      className={mergeClasses('bg-default pl-2.5 pr-3 border border-default shadow-xs min-h-[40px]', className)}
+      className={mergeClasses('cmdk-trigger bg-default pl-2.5 pr-3 border border-default shadow-xs min-h-[40px]', className)}
       onClick={() => setOpen(true)}>
       <p className="text-secondary font-normal leading-normal text-xs">Search</p>
     </Button>
