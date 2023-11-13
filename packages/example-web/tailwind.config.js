78b663a7a1e66a0e02a19bcb54e4c7b687404c9e
Simek <gosimek@gmail.com>
2023-11-13 12:49:27 +0100

    one more take

--- packages/example-web/tailwind.config.js
+++ packages/example-web/tailwind.config.js
@@ -1,16 +1,16 @@
 const expoTheme = require('@expo/styleguide/tailwind');
 
+const externalPathPrefix = process.env.NODE_ENV === 'development' ? '../..' : '.';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './common/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './components/**/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
-    './pages/**/**/*.{js,ts,jsx,tsx}',
     './utils/**/*.{js,ts,jsx,tsx}',
-    '../../node_modules/@expo/styleguide/dist/**/*.{js,ts,jsx,tsx}',
-    '../../node_modules/@expo/styleguide-search-ui/dist/**/*.{js,ts}',
+    `${externalPathPrefix}/node_modules/@expo/styleguide/dist/**/*.{js,ts,jsx,tsx}`,
+    `${externalPathPrefix}/node_modules/@expo/styleguide-search-ui/dist/**/*.{js,ts}`,
   ],
   ...expoTheme,
 };
