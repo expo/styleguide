8b9a532275dbbc821e69df3f168e8a72cec0070a
Bartosz Kaszubowski <gosimek@gmail.com>
2023-11-13 13:06:08 +0100

    [example-web] fix deploy styles by adjusting TW paths (#78)

--- packages/example-web/tailwind.config.js
+++ packages/example-web/tailwind.config.js
@@ -1,5 +1,7 @@
 const expoTheme = require('@expo/styleguide/tailwind');
 
+const externalPathPrefix = process.env.NODE_ENV === 'development' ? '../..' : '.';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,8 +9,8 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './utils/**/*.{js,ts,jsx,tsx}',
-    '../../node_modules/@expo/styleguide/dist/**/*.{js,ts,jsx,tsx}',
-    '../../node_modules/@expo/styleguide-search-ui/dist/**/*.{js,ts}',
+    `${externalPathPrefix}/node_modules/@expo/styleguide/dist/**/*.{js,ts,jsx,tsx}`,
+    `${externalPathPrefix}/node_modules/@expo/styleguide-search-ui/dist/**/*.{js,ts}`,
   ],
   ...expoTheme,
 };
