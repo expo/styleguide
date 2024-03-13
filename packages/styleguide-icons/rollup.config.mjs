1f7012782f11e510398b7edcacf1f783cc93b954
Simek <gosimek@gmail.com>
2024-03-13 11:28:36 +0100

    [styleguide-icons] restructure package, experiment with bundle

--- packages/styleguide-icons/rollup.config.mjs
+++ packages/styleguide-icons/rollup.config.mjs
@@ -2,34 +2,38 @@ import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
 
-let config = [
-  {
-    input: 'index.ts',
-    output: {
-      dir: 'dist',
-      format: 'cjs',
-    },
-    plugins: [typescript(), terser()],
-    external: ['react', 'tailwind-merge'],
+const baseConfig = {
+  input: 'src/index.ts',
+  output: {
+    name: 'index',
+    dir: 'dist',
+    format: 'cjs',
+    generatedCode: 'es2015',
+    // note(simek): Preserve modules test config
+    // exports: "named",
+    // preserveModules: true,
+    // preserveModulesRoot: 'src'
   },
-];
+  treeshake: 'smallest',
+  plugins: [typescript(), terser()],
+  external: ['react', 'tailwind-merge'],
+};
 
-if (process.env.STUB) {
-  config = [
-    {
-      input: 'index-stub.js',
-      output: {
-        file: 'dist/index.js',
-        format: 'cjs',
-      },
+function getConfig() {
+  if (process.env.STUB) {
+    return  {
+      ...baseConfig,
+      input: 'src/index-stub.js',
       plugins: [
         copy({
           targets: [{ src: './stub.d.ts', dest: 'dist', rename: 'index.d.ts' }],
         }),
-      ],
-      external: ['tailwind-merge'],
-    },
-  ];
+      ]
+    };
+  }
+  return baseConfig;
 }
+
+const config = getConfig();
 
 export default config;
