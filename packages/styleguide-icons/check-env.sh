6cf9d4bde00f1f470b39efa8b69e28f280ed12ba
Bartosz Kaszubowski <gosimek@gmail.com>
2024-12-18 14:24:10 +0100

    [monorepo] migrate to yarn v4, improve deployment process (#110)

--- packages/styleguide-icons/check-env.sh
+++ packages/styleguide-icons/check-env.sh
@@ -1,5 +1,11 @@
-if [ ! -f ./.env ]; then
-    echo "⚠️ You do not have packages/styleguide-icons/.env file. Please create it from packages/styleguide-icons/.env.example and fill it with your credentials. Instead, bundling a stub for @expo/stylguide-icons that is empty. Read the README.md in packages/styleguide-icons to build the iconset."
+if [ -z "${FIGMA_TOKEN}" ] && [ ! -f ./.env ]; then
+    echo "⚠️ You do not have packages/styleguide-icons/.env file or correct environment variables set.
+
+Please create it from packages/styleguide-icons/.env.example and fill it with your credentials.
+
+Instead, bundling a stub for @expo/styleguide-icons that is empty.
+Read the README.md in packages/styleguide-icons to build the icon set.
+"
     yarn build-stub && exit 0
 else
     yarn build-icons && exit 0
