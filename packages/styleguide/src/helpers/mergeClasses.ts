4d05fe5a15fc4acf754e6501273d403541b45dff
Bartosz Kaszubowski <gosimek@gmail.com>
2025-02-11 11:26:58 +0100

    [styleguide] support custom `heading-*` classes in merger (#112)

--- packages/styleguide/src/helpers/mergeClasses.ts
+++ packages/styleguide/src/helpers/mergeClasses.ts
@@ -1,11 +1,12 @@
 import { extendTailwindMerge } from 'tailwind-merge';
 
-type AdditionalClassGroupIds = 'icon';
+type AdditionalClassGroupIds = 'icon' | 'heading';
 
 export const mergeClasses = extendTailwindMerge<AdditionalClassGroupIds>({
   extend: {
     classGroups: {
       icon: [{ icon: ['2xs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl'] }],
+      heading: [{ heading: ['xs', 'sm', 'base', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl'] }],
     },
   },
 });
