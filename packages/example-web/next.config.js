bc49ee09b29e131ccc4018431ba672b91c7e7fce
Simek <gosimek@gmail.com>
2025-03-23 09:14:28 +0100

    [search-ui] improve RND item description, fix NaNs

--- packages/example-web/next.config.js
+++ packages/example-web/next.config.js
@@ -5,6 +5,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  devIndicators: {
+    position: 'bottom-right',
+  },
   async redirects() {
     return [
       {
