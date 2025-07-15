b5c361914f31825d0df9996a83fa4fc147ab612e
Simek <gosimek@gmail.com>
2025-07-15 12:05:22 +0200

    tweaking style and values

--- packages/search-ui/src/components/ModeTab.tsx
+++ packages/search-ui/src/components/ModeTab.tsx
@@ -13,7 +13,7 @@ export function ModeTab({ label, isActive, onClick }: Props) {
       onClick={onClick}
       className={mergeClasses(
         'flex items-center text-xs px-2.5 rounded-md h-full cursor-pointer transition text-tertiary whitespace-nowrap select-none min-h-8',
-        'hocus:bg-hover hocus:scale-[1.025] dark:hocus:bg-element',
+        'hocus:bg-hover dark:hocus:bg-element',
         'active:!scale-[0.975]',
         'max-md-gutters:w-1/2 max-md-gutters:justify-center',
         isActive && 'mode-tab',
